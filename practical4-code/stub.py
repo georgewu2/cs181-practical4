0adcc11ca1f58ac80a3f9915400e67cd4dcb149d
Lisa <lswang1618@gmail.com>
2015-05-02 18:00:43 -0400

    change to dictionary

--- practical4-code/stub.py
+++ practical4-code/stub.py
@@ -8,13 +8,13 @@ class Learner:
 
     def __init__(self):
         self.last_state  = None
-        self.pixelsize   = 10
+        self.pixelsize   = 20
         self.screen_height = 400
         self.last_action = None
         self.last_reward = None
         self.learning_rate = 1
-        self.decay_rate = 0.5
-        self.Q = np.zeros((1700,2))
+        self.decay_rate = 1
+        self.Q = dict()
 
     def reset(self):
         self.last_state  = None
@@ -24,15 +24,22 @@ class Learner:
     def convert_to_q(self, state):
         m_pos = np.floor(state['monkey']['top']/self.pixelsize)
         t_pos = np.floor(state['tree']['top']/self.pixelsize)
-        return ((self.screen_height/self.pixelsize)*m_pos + t_pos)
+        return ((self.screen_height/self.pixelsize)*(m_pos - t_pos), state['tree']['dist'])
 
     def update_Q(self, s, a):
         if self.last_state == None:
             return
         prev_state_index = self.convert_to_q(self.last_state)
         current_state_index = self.convert_to_q(s)
-        self.Q[prev_state_index][a] += self.learning_rate * (self.last_reward + self.decay_rate * (max(self.Q[current_state_index][0], self.Q[current_state_index][1])) - self.Q[prev_state_index][a])
-
+        if current_state_index in self.Q:
+            q_value = max(self.Q[current_state_index][0], self.Q[current_state_index][1])
+        else:   
+            q_value = 0
+        if prev_state_index in self.Q:
+            self.Q[prev_state_index][a] += self.learning_rate * (self.last_reward + self.decay_rate * q_value - self.Q[prev_state_index][a])
+        else:
+            self.Q[prev_state_index] = [0,0]
+            self.Q[prev_state_index][a] = self.learning_rate * (self.last_reward + self.decay_rate * q_value - self.Q[prev_state_index][a])
     def action_callback(self, state):
         '''Implement this function to learn things and take actions.
         Return 0 if you don't want to jump and 1 if you do.'''
@@ -41,18 +48,15 @@ class Learner:
 
         # You'll need to take an action, too, and return it.
         # Return 0 to swing and 1 to jump.
-        qs = [self.Q[convert_to_q(state)][a] for a in [0,1]]
-        self.Q[convert_to_q(self.last_state)][self.last_action] = self.last_reward + max(qs)
-        # Q-Learning
-        q = [self.Q[convert_to_q(state)][a] for a in [0,1]]
-        new_action = q.index(max(Q))
+       
         self.update_Q(state, self.last_action)
 
         index = self.convert_to_q(state)
-        if self.Q[index][0] > self.Q[index][1]:
-            new_action = 0
-        elif self.Q[index][0] < self.Q[index][1]:
-            new_action = 1
+        if index in self.Q and 0 in self.Q[index] and 1 in self.Q[index]:
+            if self.Q[index][0] > self.Q[index][1]:
+                new_action = 0
+            elif self.Q[index][0] < self.Q[index][1]:
+                new_action = 1
         else:
             new_action = npr.rand() < 0.1
         new_state  = state
